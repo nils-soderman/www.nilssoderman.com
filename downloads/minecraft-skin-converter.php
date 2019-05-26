fac8b5d74616dbe8323d19e01645e9b83f7ab0f0
Nils Soderman <contact@nilssoderman.com>
2019-05-26 14:05:28 +0200

    Moved files
    Moved all of the files up one dir

---
+++ downloads/minecraft-skin-converter.php
@@ -0,0 +1,76 @@
+<head>
+
+  <meta charset="UTF-8">
+  <title>Minecraft Skin Converter v1.8 - Nils Söderman - Animation Reel</title>
+  <link href="https://nilssoderman.com/resources/images/Fav_Icon.png" rel="shortcut icon" type="image/x-icon">
+  <link href="https://fonts.googleapis.com/css?family=Dosis" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="./../resources/css/main.css">
+  <link rel="stylesheet" type="text/css" href="./../resources/css/download-button.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+  <meta name="description" content="Easily convert your 1.7 skin to a 1.8 skin. There is no installation required, simply run the .jar file and a small window will pop up, from there you can type in your username or load an image from your computer. Once you’ve loaded the image, press “Convert” and volià! You now have a 1.8 skin.">
+  <meta name="keywords" content="minecraft skin converter, minecraft skin, minecraft skin converter 1.7 to 1.8, minecraft skin converter 1.8 to 1.7, minecraft skin converter download">
+  <style>
+
+    /* Page Content */
+
+    .videoWrapper {
+    	position: relative;
+    	padding-bottom: 56.25%; /* 16:9 */
+      max-width: 1280px;
+      margin: 0 auto;
+    }
+
+    .videoWrapper iframe {
+      z-index: 3;
+    	position: absolute;
+    	top: 0;
+    	left: 0;
+      margin: 0 auto;
+    	width: 100%;
+    	height: 100%;
+      max-height: 720px;
+      max-width: 1280px;
+      border-style: solid;
+      border-width: 2px 2px 2px 2px;
+      border-color: #000000;
+      background-color: #000000;
+    }
+
+    #Content {
+      margin-top: 20px;
+      max-height: 720px;
+    }
+    
+
+  </style>
+
+</head>
+
+
+<body>
+  <?php include('./../resources/includes/header.html'); ?>
+
+
+    <div id="Content">
+    
+        <div class="skin-converter-download">
+            <div class="download-button-wrapper">
+                <a href="https://nilssoderman.com/files/minecraft/Minecraft_1.8_Skin_Converter.zip">
+                    <div class="download-button">
+                        <i class="dl-image material-icons">get_app</i>
+                        <span class="dl-text">
+                            Download Minecraft Skin Converter v1.8
+                        </span>
+                        
+                    </div>
+                </a>
+            </div>
+          
+          
+        </div>
+
+    </div>
+
+    <?php include('./../resources/includes/footer.html'); ?>
+
+</body>
