95ddca8680f232fa5c4f41c928643c4b18139d54
Nils Soderman <contact@nilssoderman.com>
2019-05-26 13:51:20 +0200

    Removed symbols at the end of each line

--- Website/project/poliigon-material-converter.php
+++ Website/project/poliigon-material-converter.php
@@ -1,75 +1,75 @@
-<head>
-
-  <meta charset="UTF-8">
-  <title>Nils Söderman - Poliigon Material Converter</title>
-  <link href="https://nilssoderman.com/resources/images/icons/voice-of-the-monolith.ico" rel="shortcut icon" type="image/x-icon">
-  <link href="https://fonts.googleapis.com/css?family=Dosis" rel="stylesheet">
-  <link href="https://fonts.googleapis.com/css?family=Teko" rel="stylesheet">
-  <link rel="stylesheet" type="text/css" href="./../resources/css/main.css">
-  <link rel="stylesheet" type="text/css" href="./../resources/css/video_wrapper.css">
-  <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-
-  <style>
-
-    /* Page Content */
-
-    #Content {
-      margin-top: 20px;
-    }
-
-    #project-description{
-      margin-top: 35px;
-      font-size: 18px;
-    }
-
-    #title {
-      margin-top: 50px;
-	  font-family: 'Teko', sans-serif;
-      margin-bottom: 20px;
-	  font-size:25px;
-      text-align: center;
-    }
-
-    #description{
-      font-family: 'Roboto', sans-serif;
-      max-width: 1280px;
-      margin: 0 auto;
-      margin-bottom: 100px;
-    }
-    
-    #menu-item-projects {
-        text-decoration:underline;
-    }
-
-  </style>
-
-</head>
-
-
-<body>
-
-    <?php include('./../resources/includes/header.html'); ?>
-
-      <div id="title">
-        <h1>Poliigon Material Converter</h1>
-      </div>
-
-      <div id="description">
-        <div id="project-description">
-          <p>
-            Unreal Engine 4 - C++<br>
-            Unity - C#<br>
-            Maya - Python<br>
-            Cinema 4D - Python<br>
-            3ds Max - Maxscript<br>
-            <br>
-            
-            
-            
-          </p>
-        </div>
-      </div>
-
-    <?php include('./../resources/includes/footer.html'); ?>
-
-</body>
+<head>
+
+  <meta charset="UTF-8">
+  <title>Nils Söderman - Poliigon Material Converter</title>
+  <link href="https://nilssoderman.com/resources/images/icons/voice-of-the-monolith.ico" rel="shortcut icon" type="image/x-icon">
+  <link href="https://fonts.googleapis.com/css?family=Dosis" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Teko" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="./../resources/css/main.css">
+  <link rel="stylesheet" type="text/css" href="./../resources/css/video_wrapper.css">
+  <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+
+  <style>
+
+    /* Page Content */
+
+    #Content {
+      margin-top: 20px;
+    }
+
+    #project-description{
+      margin-top: 35px;
+      font-size: 18px;
+    }
+
+    #title {
+      margin-top: 50px;
+	  font-family: 'Teko', sans-serif;
+      margin-bottom: 20px;
+	  font-size:25px;
+      text-align: center;
+    }
+
+    #description{
+      font-family: 'Roboto', sans-serif;
+      max-width: 1280px;
+      margin: 0 auto;
+      margin-bottom: 100px;
+    }
+
+    #menu-item-projects {
+        text-decoration:underline;
+    }
+
+  </style>
+
+</head>
+
+
+<body>
+
+    <?php include('./../resources/includes/header.html'); ?>
+
+      <div id="title">
+        <h1>Poliigon Material Converter</h1>
+      </div>
+
+      <div id="description">
+        <div id="project-description">
+          <p>
+            Unreal Engine 4 - C++<br>
+            Unity - C#<br>
+            Maya - Python<br>
+            Cinema 4D - Python<br>
+            3ds Max - Maxscript<br>
+            <br>
+
+
+
+          </p>
+        </div>
+      </div>
+
+    <?php include('./../resources/includes/footer.html'); ?>
+
+</body>
