515c09a123e851f968b1ec18c92423d1f177f36f
Nils Soderman <contact@nilssoderman.com>
2019-09-08 11:03:40 +0200

    Update projects.php

--- projects.php
+++ projects.php
@@ -18,7 +18,7 @@
       margin-top: 50px;
       min-height: 500px;
     }
-    
+
     .poster {
         margin-left: 3%;
         margin-right: 0%;
@@ -28,96 +28,61 @@
     }
 
     .poster img{
-
       margin-top: 50px;
-
       border-radius: 2px;
-
       -webkit-transition: opacity .35s;
-
       transition: opacity .35s;
-
     }
 
     .poster img:hover{
-
       opacity: 0.8;
-
     }
 
-
-
     .poster img:active{
-
       opacity: 0.6;
-
     }
 
     #posters-content {
       max-width: 1280px;
-
       margin: 0 auto;
-
     }
 
     @media (max-width: 1322px) {
-
       .poster {
-
         margin-left: 7.5%;
-
       }
-
     }
 
     @media (max-width: 1127px) {
-
       .poster {
-
         margin-left: 14%;
-
       }
-
     }
 
     @media (max-width: 811px) {
-
       .poster {
-
         margin-left: 30%;
-
       }
-
     }
 
     @media (max-width: 580px) {
-
       .poster {
-
         margin-left: 18%;
-
       }
-
     }
 
-
-
     #menu-item-projects {
-
         text-decoration: underline;
-
     }
 
   </style>
-
 </head>
 
 <body>
 
     <?php include('./resources/includes/header.html'); ?>
-
+    
     <div id="Content">
-
       <div id="posters-content">
 
           <a class="poster" href="https://nilssoderman.com/project/altiia-and-the-trial-islands"><img src="./resources/images/poster/Poster_Altiia-and-the-Trial-Islands.jpg" width=256 height=362 alt="Altiia and the Trial Islands"></a>
@@ -129,7 +94,6 @@
   		  <a class="poster" href="https://nilssoderman.com/project/mocap-project"><img src="./resources/images/poster/Mocap-Scene.jpg" width=256 height=362 alt="UE4 Motion Capture Scene"></a>
 
       </div>
-
     </div>
 
     <?php include('./resources/includes/footer.html'); ?>
