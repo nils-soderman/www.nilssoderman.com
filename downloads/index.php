e2a7c9d53f5448c9cc6ad1ec7b047de03fb68542
Nils Soderman <contact@nilssoderman.com>
2019-09-08 17:19:00 +0200

    Added a index.php to downloads

---
+++ downloads/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<!--
+The source code for this website is avaliable on GitHub:
+https://github.com/nils-soderman/www.nilssoderman.com
+-->
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Nils Söderman - Projects</title>
+  <meta name="description" content="Here are some the games and other projects I've worked on recently. Altiia and the Trial Islands, Voice of the Monolith, Dodge Golf and more.">
+  <meta name="keywords" content="game project, future games, future games stockholm, student games">
+
+  <link href="./../favicon.ico" rel="shortcut icon" type="image/x-icon">
+  <link href="https://fonts.googleapis.com/css?family=Dosis" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="./../resources/css/main.css">
+  
+</head>
+
+<body>
+
+    <?php include('./../resources/includes/header.html'); ?>
+    
+    <div id="Content">
+
+
+    
+    </div>
+
+    <?php include('./../resources/includes/footer.html'); ?>
+
+</body>
+
+</html>
+
