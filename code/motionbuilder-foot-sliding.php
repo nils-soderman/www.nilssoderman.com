b6f57a5f3a3da021bc7c04cfcd34ef777339ba0e
Nils Soderman <contact@nilssoderman.com>
2019-09-10 21:54:24 +0200

    added viewport meta tag

--- code/motionbuilder-foot-sliding.php
+++ code/motionbuilder-foot-sliding.php
@@ -1,97 +1,100 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-
-  <meta charset="UTF-8">
-  <title>MotionBuilder Foot Sliding Python Script - Nils Söderman</title>
-  <link href="./../resources/images/icons/source-code-icon.ico" rel="shortcut icon" type="image/x-icon">
-  <link href="https://fonts.googleapis.com/css?family=Dosis" rel="stylesheet">
-  <link href="https://fonts.googleapis.com/css?family=Source+Code+Pro&display=swap" rel="stylesheet">
-  <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
-  <link rel="stylesheet" type="text/css" href="./../resources/css/main.css">
-  <link rel="stylesheet" type="text/css" href="./../resources/css/download-button.css">
-  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-  <meta name="description" content="Foot Sliding script">
-  <meta name="keywords" content="python, motionbuilder, sciprt, animation, foot, feet, sliding, tech, technical, code, source, pyfbsdk, CharacterSelectAndKey">
-  
-  <style>
-
-    #Content {
-      margin-top: 20px;
-      max-height: 1500px;
-      max-width:800px;
-      margin: 0 auto;
-    }
-    
-    .code-block {
-        background-color: #eaeaea;
-        border-style: solid;
-        border-width: 2px 2px 2px 2px;
-        border-color: #6b6b6b;
-    }
-    
-    code *::selection{
-        background-color: #b7dff7;
-    }
-    
-    code::selection{
-        background-color: #b7dff7;
-    }
-    
-    .code-block pre {
-        margin:10px;
-        font-family: 'Source Code Pro', monospace;
-    }
-    
-    #post-code {
-        margin-top: 20px;
-    }
-    
-    #title{
-        text-align: center;
-        margin-top: 50px;
-        font-family: 'Roboto', sans-serif;
-        
-    }
-
-  </style>
-
-</head>
-
-
-<body>
-    
-    <?php include('./../resources/includes/header.html'); ?>
-    <div id="Content">
-        
-        <div id="title">
-            <h2>MotionBuilder Feet Sliding Script</h2>
-            <h3>Laungage: Python</h3>
-        </div>
-        
-        <div class="code-block">
-            <pre><code class="SyntaxHighlight"><?php include('./../resources/scripts/motionbuilder/Feet_Sliding.py'); ?></code></pre>
-        </div>
-        
-        <div id = "post-code" >
-            <div id="download-button">
-                <div class="download-button-wrapper">
-                    <a href="./../resources/scripts/motionbuilder/Feet_Sliding.py" download="Feet_Sliding.py", target="_blank">
-                        <div class="download-button">
-                            <i class="dl-image material-icons">get_app</i>
-                            <span class="dl-text">
-                                Download Feet-Sliding.py
-                            </span>
-                        </div>
-                    </a>
-                </div>
-            </div>
-        </div>
-    </div>
-    <?php include('./../resources/includes/footer.html'); ?>
-    
-    <script type="text/javascript" src="./../resources/scripts/SyntaxHighlighter.js"></script>
-
-</body>
-
-</html>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="UTF-8">
+  <title>MotionBuilder Foot Sliding Python Script - Nils Söderman</title>
+  <meta name="description" content="Foot Sliding script">
+  <meta name="keywords" content="python, motionbuilder, sciprt, animation, foot, feet, sliding, tech, technical, code, source, pyfbsdk, CharacterSelectAndKey">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+  <link href="./../resources/images/icons/source-code-icon.ico" rel="shortcut icon" type="image/x-icon">
+  <link href="https://fonts.googleapis.com/css?family=Dosis" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Source+Code+Pro&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="./../resources/css/main.css">
+  <link rel="stylesheet" type="text/css" href="./../resources/css/download-button.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+  
+  <style>
+
+    #Content {
+      margin-top: 20px;
+      max-height: 1500px;
+      max-width:800px;
+      margin: 0 auto;
+    }
+
+    .code-block {
+        background-color: #eaeaea;
+        border-style: solid;
+        border-width: 2px 2px 2px 2px;
+        border-color: #6b6b6b;
+    }
+
+    code *::selection{
+        background-color: #b7dff7;
+    }
+
+    code::selection{
+        background-color: #b7dff7;
+    }
+
+    .code-block pre {
+        margin:10px;
+        font-family: 'Source Code Pro', monospace;
+    }
+
+    #post-code {
+        margin-top: 20px;
+    }
+
+    #title{
+        text-align: center;
+        margin-top: 50px;
+        font-family: 'Roboto', sans-serif;
+    }
+
+  </style>
+
+</head>
+
+<body>
+
+    <?php include('./../resources/includes/header.html'); ?>
+    <div id="Content">       
+
+        <div id="title">
+            <h2>MotionBuilder Feet Sliding Script</h2>
+            <h3>Laungage: Python</h3>
+        </div>
+
+        <div class="code-block">
+            <pre><code class="SyntaxHighlight"><?php include('./../resources/scripts/motionbuilder/Feet_Sliding.py'); ?></code></pre>
+        </div>
+
+        <div id = "post-code" >
+            <div id="download-button">
+                <div class="download-button-wrapper">
+                    <a href="./../resources/scripts/motionbuilder/Feet_Sliding.py" download="Feet_Sliding.py", target="_blank">
+                        <div class="download-button">
+                            <i class="dl-image material-icons">get_app</i>
+                            <span class="dl-text">Download Feet-Sliding.py</span>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <?php include('./../resources/includes/footer.html'); ?>
+
+    <script type="text/javascript" src="./../resources/scripts/SyntaxHighlighter.js"></script>
+
+</body>
+
+
+
+</html>
+
