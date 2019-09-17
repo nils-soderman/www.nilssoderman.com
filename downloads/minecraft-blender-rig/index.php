7fbc692d216bfa61b9f7ab552adf176d1c336d6e
Nils Soderman <contact@nilssoderman.com>
2019-09-17 22:07:11 +0200

    Minimized css & added meta data for sharing on social media

--- downloads/minecraft-blender-rig/index.php
+++ downloads/minecraft-blender-rig/index.php
@@ -6,18 +6,23 @@ https://github.com/nils-soderman/www.nilssoderman.com
 <html lang="en">
 <head>
 
-  <meta charset="UTF-8">
-  <title>Minecraft Blender Rig Download - Rymdnisse</title>
-  <meta name="description" content="A free Minecraft rig pack for Blender containing Blocks, Items, Mobs and a Character rig fully ready to be animated.">
-  <meta name="keywords" content="minecraft, blender, rig, download, free, render, animation, pack, mob, steve, rymdnisse">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  
-  <link href="./../../resources/images/icons/icon-minecraft-blender-rig.ico" rel="shortcut icon" type="image/x-icon">
-  <link href="https://fonts.googleapis.com/css?family=Dosis" rel="stylesheet">
-  <link rel="stylesheet" type="text/css" href="./../../resources/css/main.min.css">
-  <link rel="stylesheet" type="text/css" href="./../../resources/css/download-button.css">
-  <link rel="stylesheet" type="text/css" href="./../../resources/css/pages/downloads/minecraft-blender-rig.css">
-  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <meta charset="UTF-8">
+    <title>Minecraft Blender Rig Download - Rymdnisse</title>
+    <meta name="description" content="A free Minecraft rig pack for Blender containing Blocks, Items, Mobs and a Character rig fully ready to be animated."></meta>
+    <meta name="keywords" content="minecraft, blender, rig, download, free, render, animation, pack, mob, steve, rymdnisse"></meta>
+    <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+
+    <meta name="og:title" content="Minecraft Blender Rig Pack - Download"></meta>
+    <meta name="og:description" content="A free Minecraft rig pack for Blender containing Blocks, Items, Mobs and a Character rig fully ready to be animated."></meta>
+    <meta name="og:image" content="https://nilssoderman.com/resources/images/image/minecraft-blender-rig-image.jpg"></meta>
+    <meta name="twitter:card" content="summary"></meta>
+
+    <link href="./../../resources/images/icons/icon-minecraft-blender-rig.ico" rel="shortcut icon" type="image/x-icon">
+    <link href="https://fonts.googleapis.com/css?family=Dosis" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="./../../resources/css/main.min.css">
+    <link rel="stylesheet" type="text/css" href="./../../resources/css/download-button.min.css">
+    <link rel="stylesheet" type="text/css" href="./../../resources/css/pages/downloads/minecraft-blender-rig.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 
   
 
@@ -80,11 +85,20 @@ https://github.com/nils-soderman/www.nilssoderman.com
             </div>
         </div>
 
+        <div id="bss-dl-page">
+            <a href="https://www.boxscapestudios.tk/downloads" target="_blank">Boxscape Studios download page</a>
+        </div>
+
         <h5 class="dl-title faq-title">What is the BSS Edit?</h5>
             <p class="faq-text">Back in 2017 I handed over future development of this rig pack to Boxscape Studios.<br>
             Since then they have done an absolutely fantastic job at keeping the rig pack up to date and adding a bunch of cool new things.<br>
             You can visit them at Boxscape Studios: <a href="https://www.boxscapestudios.tk" target="_blank">Website</a>, <a href="https://twitter.com/BoxscapeStudios" target="_blank">Twitter</a>
             </p>
+
+        <h5 class="dl-title faq-title">Does this rig work with Blender 2.8?</h5>
+        <p class="faq-text">Yes. Both the Cycles version and the BSS Edit works in Blender 2.8 and above.<br>
+        The Internal version will no longer work as the internal render engine has been removed from Blender 2.8.
+        </p>
     </div>
 
 
