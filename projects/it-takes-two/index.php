c9bbb7e18fe76117646f8284a24b2b43826736c3
nils-soderman <rymdnisse@live.com>
2020-07-05 18:15:52 +0200

    Added project It Takes Two

---
+++ projects/it-takes-two/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<!--
+The source code for this website is available on GitHub:
+https://github.com/nils-soderman/www.nilssoderman.com
+-->
+<html lang="en">
+<head>   
+    <meta charset="UTF-8">
+    <title>It Takes Two - Nils Söderman</title>
+    <meta name="description" content="Breakdown of the animations done for the Altiia and the Trial Islands game project. This was a student project made at Futuregames and the game was made in Unity.">   
+    <meta name="keywords" content="unity animation example, game animation breakdown, futuregames, free open world game, student game">   
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <link rel="stylesheet" type="text/css" href="./../../resources/css/main.min.css">  
+    <link rel="stylesheet" type="text/css" href="./../../resources/css/video_wrapper.css">   
+    <link rel="stylesheet" type="text/css" href="./../../resources/css/download-button.min.css">
+    <link rel="stylesheet" type="text/css" href="./stylesheet.min.css">
+    <link href="https://nilssoderman.com/resources/images/icons/altiia-and-the-trial-islands-icon.ico" rel="shortcut icon" type="image/x-icon">
+</head>
+
+
+<body>
+
+    <?php include('./../../resources/includes/header.html'); ?>
+
+    <div id="Content">
+
+      <div id="title">
+        <h1>It Takes Two</h1>
+      </div>
+
+      <div class="videoWrapper">
+        <iframe width="1280" height="720" src="https://www.youtube.com/embed/blwlcwlAXwA?rel=0&amp;showinfo=0" allowfullscreen></iframe>
+      </div>
+
+      <div id="description">
+        <div id="project-description">
+            <p>
+                Comming 2021...            
+            </p>
+        </div>
+        </div>
+    </div>
+
+  <?php include('./../../resources/includes/footer.html'); ?>
+
+</body>
+
+</html>
