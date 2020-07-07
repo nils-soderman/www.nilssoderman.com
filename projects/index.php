7b68ddf78b265842c052400308f7d61c8b083974
nils-soderman <rymdnisse@live.com>
2020-07-07 13:02:46 +0200

    The posters are now placed in a horizontal scrollbox

--- projects/index.php
+++ projects/index.php
@@ -18,21 +18,35 @@ https://github.com/nils-soderman/www.nilssoderman.com
 
     /* Page Content */
 
-    #Content {
+    #content {
       margin-top: 50px;
-      min-height: 500px;
+      min-height: 450px;
+    }
+
+    #posters{
+      margin-bottom: 10px;
+      user-select: none;
+      -moz-user-select: none;
+      -webkit-user-drag: none;
+      -webkit-user-select: none;
+      -ms-user-select: none;
+    }
+
+    #posters-scrollbox {
+      white-space: nowrap;
+      overflow-x: auto;
     }
 
     .poster {
-        margin-left: 3%;
-        margin-right: 0%;
-        margin-top: 20px;
-        margin-bottom: 50px;
-        height: 5000px;
+        margin-right: 3%;
+    }
+
+    #poster-empty-space{
+      width:3%;
+      display:inline-block;
     }
 
     .poster img{
-      margin-top: 50px;
       border-radius: 2px;
       -webkit-transition: opacity .35s;
       transition: opacity .35s;
@@ -46,38 +60,75 @@ https://github.com/nils-soderman/www.nilssoderman.com
       opacity: 0.6;
     }
 
+    .posters-border-fade{
+      height: 98%;
+      width: 20px;
+      position:absolute;
+      top:0;
+      z-index:2;
+    }
+
+    .fade-right{
+      right:0;
+      background: linear-gradient(to left, #e3e3e3, #e3e3e300 100%);
+    }
+    
+    .fade-left{
+      left:0;
+      background: linear-gradient(to right, #e3e3e3, #e3e3e300 100%);
+    }
+
     #posters-content {
-      max-width: 1280px;
-      margin: 0 auto;
+      margin-top:80px;
+      position:relative;
     }
 
-    @media (max-width: 1322px) {
-      .poster {
-        margin-left: 7.5%;
-      }
+    /* width */
+    #posters-scrollbox::-webkit-scrollbar {
+      margin-top: 15px;
+      height: 5px;
     }
 
-    @media (max-width: 1127px) {
-      .poster {
-        margin-left: 14%;
-      }
+    /* Handle */
+    #posters-scrollbox::-webkit-scrollbar-thumb {
+      background: #888;
+      border-radius: 2px;
     }
 
-    @media (max-width: 811px) {
-      .poster {
-        margin-left: 30%;
-      }
+    /* Handle on hover */
+    #posters-scrollbox::-webkit-scrollbar-thumb:hover {
+      background: #555;
     }
 
-    @media (max-width: 580px) {
-      .poster {
-        margin-left: 18%;
-      }
-    }
 
     #menu-item-projects {
         text-decoration: underline;
     }
+
+
+    @media (max-width: 500px)
+    {
+      .poster img{
+        width: 200px;
+        height: auto;
+      }
+
+      #posters-content {
+        margin-top:10px;
+      }
+      
+      #content {
+        margin-top: 50px;
+        min-height: 300px;
+      }
+
+      .posters-border-fade{
+        width: 10px;
+      }
+
+
+    }
+    
 
   </style>
 </head>
@@ -86,20 +137,65 @@ https://github.com/nils-soderman/www.nilssoderman.com
 
     <?php include('./../resources/includes/header.html'); ?>
     
-    <div id="Content">
+    <div id="content">
       <div id="posters-content">
-
-          <a class="poster" href=""><img src="./../resources/images/poster/poster-it-takes-two.jpg" width=256 height=362 alt="It Takes Two - Hazelight"></a>
-
-          <a class="poster" href="./altiia-and-the-trial-islands/"><img src="./../resources/images/poster/Poster_Altiia-and-the-Trial-Islands.jpg" width=256 height=362 alt="Altiia and the Trial Islands"></a>
-
-          <a class="poster" href="./voice-of-the-monolith/"><img src="./../resources/images/poster/Poster_Voice-of-the-Monolith.jpg" width=256 height=362 alt="Voice of the Monolith"></a>
-
-          <a class="poster" href="./dodge-golf/"><img src="./../resources/images/poster/Poster_Dodge-Golf.jpg" width=256 height=362 alt="Dodge Golf"></a>
+        <div class="posters-border-fade fade-left"></div>
+        <div id="posters-scrollbox">
+          <div id="posters">
+            <div id="poster-empty-space"></div>
+            <a class="poster" href=""><img src="./../resources/images/poster/poster-it-takes-two.jpg" width=256 height=362 alt="It Takes Two - Hazelight"></a>
+            <a class="poster" href="./altiia-and-the-trial-islands/"><img src="./../resources/images/poster/Poster_Altiia-and-the-Trial-Islands.jpg" width=256 height=362 alt="Altiia and the Trial Islands"></a>
+            <a class="poster" href="./voice-of-the-monolith/"><img src="./../resources/images/poster/Poster_Voice-of-the-Monolith.jpg" width=256 height=362 alt="Voice of the Monolith"></a>
+            <a class="poster" href="./dodge-golf/"><img src="./../resources/images/poster/Poster_Dodge-Golf.jpg" width=256 height=362 alt="Dodge Golf"></a>
+          </div>
+        </div>
+        <div class="posters-border-fade fade-right"></div>
       </div>
     </div>
 
     <?php include('./../resources/includes/footer.html'); ?>
+
+    <script>
+    // Make the posters dragable left/right
+    
+    const PosterArea = document.getElementById("posters-scrollbox");
+    var bDragging = false;
+    var bMouseDown = false;
+    var StartPosX;
+    var CurrentScroll;
+
+    PosterArea.addEventListener('mousedown', (Event) => {
+      bDragging = false;
+      bMouseDown = true;
+      StartPosX = Event.pageX - PosterArea.offsetLeft;
+      CurrentScroll = PosterArea.scrollLeft;
+    });
+    document.addEventListener('mouseup', (Event) => {
+      bMouseDown = false;
+    });
+    PosterArea.addEventListener('click', (Event) => {
+      if(bDragging)
+      {
+        Event.preventDefault();
+      }
+      bDragging = false;
+    });
+    document.addEventListener('mousemove', (Event) => {
+      if(!bMouseDown) return;
+      Event.preventDefault();
+      const x = Event.pageX - PosterArea.offsetLeft;
+      const AmountMoved = (x - StartPosX);
+      if (AmountMoved > 10 || AmountMoved < -10){
+        bDragging = true;
+      }
+      if (bDragging){
+        PosterArea.scrollLeft = CurrentScroll - ((x - StartPosX) * 1.5);
+      }
+    });
+    
+    
+    </script>
+
 
 </body>
 
